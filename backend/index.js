098d1de17f672693789f2bf4b70200f7aa74d52f
sauravs4h <sauravs4h@gmail.com>
2023-04-04 10:44:18 +0530

    signup in progress

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,26 @@
-const express=require("express")
+const express=require("express");
+var cors = require('cors')
 const app=express();
 
 
-const {connection}=require("./config/db")
+const {connection}=require("./config/db");
+const {users}=require("./routes/user.routes")
+
+
+app.use(cors())
+app.use(express.json());
+app.use("/user",users)
+
+
+
+
 
 app.get("/",(req,res)=>{
     res.send({msg:"base api"})
 })
+
+
+
 
 app.listen(8080,async()=>{
     try {
