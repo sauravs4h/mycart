ae5d620e7d3179fc8152aa4be7d89583f364b65e
sauravs4h <sauravs4h@gmail.com>
2023-04-04 10:26:00 +0530

    usermodel

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -1,2 +1,24 @@
 const mongoose=require("mongoose");
 
+const userSchema=mongoose.Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true
+    },
+    mobile_no:{
+        type:Number,
+        require:true
+    },
+    password:{
+        type:String,
+        require:true
+    }
+})
+
+const Usermodel=mongoose.model("Users",userSchema);
+
+module.exports={Usermodel}
