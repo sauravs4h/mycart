1972797305abf526fce3c4306964cb44302d9f2e
sauravs4h <sauravs4h@gmail.com>
2023-04-04 10:06:29 +0530

    setup

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 const express=require("express")
-
 const app=express();
 
+
+const {connection}=require("./config/db")
 
 app.get("/",(req,res)=>{
     res.send({msg:"base api"})
 })
 
-app.listen(8080,()=>{
+app.listen(8080,async()=>{
+    try {
+        await connection
+        console.log("connected to db")
+    } catch (error) {
+        console.log("not connected to db")
+    }
     console.log("listining at 8080");
 })
