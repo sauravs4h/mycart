e8c284038e1224e7f4dde1f60942f9a79bd54524
sauravs4h <sauravs4h@gmail.com>
2023-04-05 10:37:27 +0530

    admin side progress

---
+++ frontend/Admin-side/script/product.js
@@ -0,0 +1,70 @@
+
+
+const api="http://localhost:8080/product"
+
+//http://localhost:8080/product/allproduct
+
+const getproduct=async()=>{
+
+    try {
+
+        const res=await fetch(`${api}/allproduct`,{
+            method:"GET",
+            headers:{
+                "content-type":"application/json"
+            }
+        })
+
+         result=await res.json()
+        result=result.products
+         console.log(result)
+
+         appendproduct(result)
+        
+    } catch (error) {
+        
+    }
+}
+
+getproduct()
+
+
+product_div=document.getElementById("allproduct");
+
+
+const appendproduct=(res)=>{
+
+    product_div.innerHTML="";
+    res.forEach((el)=>{
+
+        let div=document.createElement("div")
+        
+        let img=document.createElement("img")
+        img.src=el.image
+
+        let name=document.createElement("h2")
+        name.innerText=el.name
+
+        let price=document.createElement("h3")
+        price.innerText=el.price
+
+        let brand=document.createElement("h3")
+        brand.innerText=el.brand
+
+        let category=document.createElement("h3")
+        category.innerText=el.category
+
+        let desc=document.createElement("p")
+        desc.innerText=el.description
+
+        let update=document.createElement("button")
+        update.innerText="UPDATE"
+
+        let deletee = document.createElement("button")
+        deletee.innerText="DELETE"
+
+        div.append(img,name,price,brand,category,desc,update,deletee)
+
+        product_div.append(div)
+    })
+}
