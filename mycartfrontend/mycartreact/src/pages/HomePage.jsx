afd763911ac709732c87c6c2ed2b1bf311a7f6d4
sauravs4h <sauravs4h@gmail.com>
2024-02-21 10:56:45 +0530

    setup frontend with react

---
+++ mycartfrontend/mycartreact/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function HomePage() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Home page</h1>
+    </div>
+  );
+}
+
+export default HomePage;
